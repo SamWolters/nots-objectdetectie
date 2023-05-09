c5651d6c6d43d3b018fb51d91cd53108641f0904
SWLdeBruin <sjoerddebruin1@hotmail.com>
2023-05-09 17:31:19 +0200

    Split frontend and backend

---
+++ backend/Common/IClassifier.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace Common
+{
+    public interface IClassifier
+    {
+        public static abstract KeyValuePair<string, float?> Predict(Image image);
+    }
+}
---
+++ backend/Common/PredictionObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class PredictionObject
+    {
+        public string Label { get; set; }
+        public float Confidence { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public bool Warning { get; set; }
+        public string WarningMessage { get; set; }
+    }
+}
