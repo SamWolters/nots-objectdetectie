786a6149076f7a735ed6c62ebe613fac093cd461
MM-G-hue <marijn.martin@hotmail.com>
2023-05-23 17:10:53 +0200

    backend update for dynamic loading of models

---
+++ backend/DogClassifier/DogsClassifier.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Classifiers
+{
+    public class DogsClassifier : IClassifier
+    {
+        public string ClassifiesObject { get; } = "dog";
+
+        public KeyValuePair<string, float?> Predict(Image image)
+        {
+            using MemoryStream ms = new();
+            image.Save(ms, ImageFormat.Jpeg);
+
+            var input = new ModelInput()
+            {
+                ImageSource = ms.ToArray(),
+            };
+
+            return Dogs.PredictAllLabels(input).First();
+        }
+    }
+}
