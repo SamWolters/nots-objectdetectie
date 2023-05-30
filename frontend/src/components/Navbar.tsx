9e633cec70c3fc4cf192a1f836ad010ce1b44604
Sven van Ee <S.vanEe1@student.han.nl>
2023-05-30 11:47:33 +0200

    Homepage & Navbar added

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+
+import { Link } from "react-router-dom";
+import { LogoSVG } from "./LogoSVG";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+      <LogoSVG />
+        <Link className="navbar-brand fs-4" to="/">
+          Object Detective
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse fs-6" id="navbarNav">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/webcam">
+                Webcam
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active btn btn-primary text-white fs-6"
+                aria-current="page"
+                to="/upload"
+              >
+                Upload
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
+export const Home = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container d-flex align-items-center justify-content-center text-center text-black">
+        <div>
+          <h1 className="fs-1 mb-5">The place for detecting objects</h1>
+          <div className="mt-5">
+            <p>Upload or use your webcam to detect the objects in images.</p>
+
+            <Link to="/upload">
+              <button className="btn btn-primary btn-lg rounded-3 mx-5">
+                Upload
+              </button>
+            </Link>
+            <Link to="/webcam">
+              <button className="btn btn-primary btn-lg rounded-3 mx-5">
+                Webcam
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
